27ff890ae23185ac97224a0e125fcf447b23f6aa
jessef <1319957+jfrausto@users.noreply.github.com>
2021-08-23 21:31:48 -0700

    load nextjs, chakra, typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,39 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Center,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/react'
+import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+
+import { Hero } from '../components/Hero'
+import { Container } from '../components/Container'
+import { Main } from '../components/Main'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+import { CTA } from '../components/CTA'
+import { Footer } from '../components/Footer'
+
+const Index = () => (
+  <Container height="100vh">
+    <Hero />
+    <Main>
+      <Text
+        bg="gray.800"
+        p={2}
+        borderRadius="full"
+      >
+        <Center>v v v v v v v v</Center>
+      </Text>
+    </Main>
+
+    <DarkModeSwitch />
+    <Footer>
+      <Text>Next ❤️ Chakra</Text>
+    </Footer>
+    <CTA />
+  </Container>
+)
+
+export default Index
